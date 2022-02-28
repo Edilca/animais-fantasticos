79c97525441b5b5b6c82a438242780abd6138f0f
Edilca <edilca@gmail.com>
2022-02-28 17:05:33 -0300

    refatorar menu mobile transformando em classes

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js';
+import MenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
@@ -18,9 +18,11 @@ const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section'
 tabNav.init();
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 Moldal.init();
+const menuMobile = new Mobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
 initTooltip();
 initDropdownMenu();
-initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
