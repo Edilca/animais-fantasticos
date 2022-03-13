0f735a2ebbd8e8d771756914289822c4afb0b934
Edilca <edilca@gmail.com>
2022-03-13 19:07:11 -0300

    funcionamento refatorado

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from './modules/modal.js';
 import MenuMobile from './modules/menu-mobile.js';
 import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
-import initFuncionamento from './modules/funcionamento.js';
+import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import ScrollAnima from './modules/scroll-anima.js';
@@ -27,9 +27,8 @@ const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
 const dropdownMenu = new DropdownMenu('[data-dropdown]');
 dropdownMenu.init();
-
-initFuncionamento();
-initFetchBitcoin();
+const funcionamento = new Funcionamento('[data-semana]', 'aberto');
+funcionamento.init();
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
